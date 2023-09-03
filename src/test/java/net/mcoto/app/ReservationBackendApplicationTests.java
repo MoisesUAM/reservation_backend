354883fdc3527f0600358b4424d6552830b60f1f
MoisesUAM <moises.coto@uamcr.net>
2023-09-02 20:33:42 -0600

    Inicio de proyecto Spring para backend de aplicacion de reservas en Andriod

---
+++ src/test/java/net/mcoto/app/ReservationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mcoto.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
